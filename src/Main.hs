33b1aa6431e4901908933f2437212081a311055f
Devin Jacobs <devrjacobs@gmail.com>
2015-02-14 14:39:43 -0700

    Completed renderBackground.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 import Graphics.Gloss (play, Display (InWindow))
 import Graphics.Gloss.Data.Color (Color, makeColor)
-import Graphics.Gloss.Data.Picture (Picture, blank)
+import Graphics.Gloss.Data.Picture
+    (Picture, pictures, blank, translate, color, thickCircle, rectangleSolid)
 import Graphics.Gloss.Interface.Pure.Game (Event)
 import Data.Map as M
 
@@ -13,7 +14,13 @@ instance Show Marker where
     show Black = "B"
 
 backgroundColor :: Color
-backgroundColor = makeColor 0.5 0.5 0.5 0
+backgroundColor = makeColor 0.5 0.5 0.5 1
+
+mainBoardColor :: Color
+mainBoardColor = makeColor 0.8 0 0 1
+
+secondaryBoardColor :: Color
+secondaryBoardColor = makeColor 0.5 0 0 1
 
 main :: IO ()
 main = play
@@ -25,8 +32,19 @@ main = play
     handleInput
     step
 
+renderQuadrant :: Picture
+renderQuadrant = pictures $
+    [ color mainBoardColor $ rectangleSolid 140 140 ] ++
+    [ color secondaryBoardColor $ translate x y $ thickCircle 5 10
+        | x <- [-50, 0, 50]
+        , y <- [-50, 0, 50] ]
+
+renderBackground :: Picture
+renderBackground = pictures $
+    [ translate x y $ renderQuadrant | x <- [-75, 75], y <- [-75, 75] ]
+
 render :: Board -> Picture
-render _ = blank
+render _ = renderBackground
 
 handleInput :: Event -> Board -> Board
 handleInput _ b = b
